289516874ea89c0f8f24407a8c88ee4094be0d98
eugenm17 <85393003+eugenm17@users.noreply.github.com>
2021-06-05 13:59:08 +0300

    Add files via upload
    Progress bar

---
+++ exemplu.py
@@ -0,0 +1,13 @@
+from tqdm import tqdm
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+d=[]
+d.append(a)
+d.append(b)
+d.append(c)
+for i in tqdm(range(0,3)):
+    for j in tqdm(range(0,3)):
+        for k in tqdm(range(0,3)):
+            if(i!=j&j!=k&k!=i):
+                print(d[i],d[j],d[k])
