5cc6334f6dfb46fa2359082fa2a222d093448126
eugenm17 <85393003+eugenm17@users.noreply.github.com>
2021-06-05 14:24:29 +0300

    Add files via upload

---
+++ exemplu3.py
@@ -0,0 +1,5 @@
+import emoji
+
+print(emoji.emojize('Impact is :thumbsup:', use_aliases=True))
+print(emoji.emojize('Students are :blush: ', use_aliases=True))
+print(emoji.demojize('Students are '))
